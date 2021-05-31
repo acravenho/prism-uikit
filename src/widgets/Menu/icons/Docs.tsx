8d99ac69972e05f379c8f1eb4f9c8181d206e420
PrismDefi <marcos.caforio89@gmail.com>
2021-05-31 18:20:26 -0300

    New Icons

---
+++ src/widgets/Menu/icons/Docs.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Svg from "../../../components/Svg/Svg";
+import { SvgProps } from "../../../components/Svg/types";
+
+const Icon: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 24 24" {...props}>
+      <path d="M11.362 2c4.156 0 2.638 6 2.638 6s6-1.65 6 2.457v11.543h-16v-20h7.362zm.827-2h-10.189v24h20v-14.386c0-2.391-6.648-9.614-9.811-9.614zm4.811 13h-10v-1h10v1zm0 2h-10v1h10v-1zm0 3h-10v1h10v-1z"/>
+    </Svg>
+  );
+};
+
+export default Icon;
